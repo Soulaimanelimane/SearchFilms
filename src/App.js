b3a295d32950b36f058aa7d4089ffd487fd07b86
soulaimane limane <limansoulaimn@gmail.com>
2024-05-16 21:50:47 +0100

    secend commit :)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
+import {  useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const SearchREF = useRef('');
+  const [data, setData] = useState([]);
+  const [pages , setPages]= useState() ; 
+  const [textEND, settextEND] = useState(false);
+  
+  const handleSearch = async () => {
+
+    const search = SearchREF.current.value;
+    const url = `https://api.themoviedb.org/3/search/tv?api_key=fef55a6754f2f6d00a0038388915039c&include_adult=false&query=${search}`;
+    
+    await fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+         setData(data.results) ; 
+         setPages(data.total_pages)
+         
+        })
+      .catch(err => console.log(err));
+  };
+
+  const limitOverview = (text) => {
+    if (text.length <= 300) {
+      return text;
+    } else {
+      return (
+        <>
+          {textEND === true ? text : `${text.slice(0, 300)}...`}
+          <button className='btn border-0' onClick={() => settextEND(textEND === true ? false : true)}>
+            {textEND === true ? 'see less' : 'see more'}
+          </button>
+        </>
+      );
+    }
+  };
+  const renderPages = (page )=>{
+    const PagesNumbers = [] ; 
+    for (let index = 1; index <= page; index++) {
+      PagesNumbers.push(
+      <button className='mx-3  pt-1 rounded-circle btn btn-outline-primary  text-center ' style={{width:35+"px"  , height:35+"px" }}>
+             {index}
+      </button>
+      )
+      
+    }
+    return(PagesNumbers);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="form">
+        <div className="my-3 d-flex justify-content-center align-items-center">
+          <input type="text"  className="form-control w-50 mx-2" ref={SearchREF}  placeholder="Search for your films here"/>
+          <button type="button" onClick={handleSearch} className="btn btn-primary mx-2">
+            Search
+          </button>
+        </div>
+      </form>  
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+          {data.map((item, key) => (
+            <div className="col" key={key}>
+              <div className="card h-100">
+                {item.backdrop_path ? (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w1280${item.backdrop_path}`}
+                    className="card-img-top"
+                    alt="found"
+                  />
+                ) : (
+                  <img src="https://imgs.search.brave.com/EUaZRwPjd_qm3jwJFaj1dip5dJTeGuILfZCzBQJCWHU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMud29uZGVyc2hh/cmUuY29tL3JlcGFp/cml0L2F0aWNsZS8y/MDIxLzA3L3Jlc29s/dmUtaW1hZ2VzLW5v/dC1zaG93aW5nLXBy/b2JsZW0tMS5qcGc"
+                    className="card-img-top"
+                    alt="not found"
+                  />
+                )}
+                <div className="card-body d-flex flex-column">
+                  <ul className="list-group list-group-flush flex-grow-1">
+                    <li className="list-group-item">
+                      <p className="font-weight-bold">{item.name}</p>
+                    </li>
+                    <li className="list-group-item flex-grow-1">
+                      {limitOverview(item.overview)}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ))}
+          
+        </div>
+       {pages ?
+        <div className=' my-3 d-flex justify-content-center align-items-center  '>
+           {renderPages(pages)}
+       </div>
+    
+       : ""} 
+      </div>
+
+      <style jsx="true">{`
+        .card-img-top {
+          height: 200px;
+          object-fit: cover;
+        }
+      `}</style>
+    
+    </>
   );
 }
 
